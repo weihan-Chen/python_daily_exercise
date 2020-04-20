2d82367b8f7572c3eb300651dfe317629501f068
weihan-Chen <weihan-Chen@qq.com>
2020-04-20 23:21:04 +0800

    fix format

--- strings_examples/理解compile的作用.py
+++ strings_examples/理解compile的作用.py
@@ -1,5 +1,5 @@
 import re
-pat = re.compile('\W+')  #\W匹配不是数字和字母的字符
+pat = re.compile('\W+')  # \W匹配不是数字和字母的字符
 has_special_chars = pat.search('ed#2@edc')
 if has_special_chars:
     print(f'str contains special characters:{has_special_chars.group(0)}')
