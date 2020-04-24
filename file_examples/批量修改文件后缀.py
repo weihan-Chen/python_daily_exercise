ef3fd3badd63c19107dd137163390812426d13da
weihan-Chen <weihan-Chen@qq.com>
2020-04-25 01:23:42 +0800

    complete

--- file_examples/批量修改文件后缀.py
+++ file_examples/批量修改文件后缀.py
@@ -27,4 +27,43 @@ def get_parser():
                         nargs=1,
                         help='新的后缀')
     return parser
-# TODO continue this py
+
+
+# 后缀名批量修改
+def batch_rename(work_dir, old_ext, new_ext):
+    '''
+    传递当前目录，原来的后缀名，新的后缀名后，批量重命名后缀名
+    '''
+    for filename in os.listdir(work_dir):
+        # 获取得到文件后缀
+        split_file = os.path.splitext(filename)
+        file_ext = split_file[1]
+        # 定位后缀名为 old_ext 的文件
+        if old_ext == file_ext:
+            # 修改后文件的完整名称
+            newfile = split_file[0] + new_ext
+            # 实现重命名操作
+            os.rename(os.path.join(work_dir, filename),
+                      os.path.join(work_dir, newfile))
+    print('rename completed!')
+    print(os.listdir(work_dir))
+
+
+# 实现 main
+def main():
+    # 命令行参数
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    # 从命令行参数中依次解析出参数
+    work_dir = args['work_dir'][0]
+    old_ext = args['old_ext'][0]
+    if old_ext[0] != '.':
+        old_ext = '.' + old_ext
+    new_ext = args['new_ext'][0]
+    if new_ext[0] != '.':
+        new_ext = '.' + new_ext
+    batch_rename(work_dir, old_ext, new_ext)
+
+
+if __name__ == '__name__':
+    main()
