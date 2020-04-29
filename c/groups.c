5ba462be4d8c42ca637ae8ef0c9fd57087f6fb96
weihan-Chen <weihan-Chen@qq.com>
2020-04-29 17:36:39 +0800

    change catolog

---
+++ c/groups.c
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: weihan-Chen
+ * @Github: https://github.com/weihan-Chen
+ * @Date: 2020-04-29 17:28:56
+ * @LastEditTime: 2020-04-29 17:34:25
+ */
+#include <stdio.h>
+void main()
+{
+    int people_summary, group_summary;
+    printf("一共的人数：");
+    scanf("%d", &people_summary);
+    return;
+}
