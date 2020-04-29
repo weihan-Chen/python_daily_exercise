1aa82dd7dd0bb9066dfe9bd0cd1deea8bce2cf30
weihan-Chen <weihan-Chen@qq.com>
2020-04-29 10:09:07 +0800

    initial commit

---
+++ examples/找出所有重复元素.py
@@ -0,0 +1,17 @@
+'''
+@Description:
+@Author: weihan-Chen
+@Github: https://github.com/weihan-Chen
+@Date: 2020-04-29 10:01:41
+@LastEditTime: 2020-04-29 10:08:09
+'''
+from collections import Counter
+
+
+def find_all_duplicates(lst):
+    c = Counter(lst)
+    print(c)
+    return list(filter(lambda k: c[k] > 1, c))
+
+
+print(find_all_duplicates([1, 2, 2, 3, 3, 3]))
